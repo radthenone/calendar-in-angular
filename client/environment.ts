f710646ac1b2a56ba9b1c369224d8ed995a4242b
radthenone <krzyzak.codes@gmail.com>
2024-12-22 02:08:30 +0100

    removed uploaded file

--- client/environment.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-  frontendUrl: 'http://localhost:4200',
-  backendUrl: 'http://localhost:8200',
-  secretKey: 'some_secret_key',
-  apiUrl: 'http://localhost:3000',
-};
